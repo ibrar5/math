e3692ce9234df242cfe6baaae07c5d89fb1ce4d7
ibrar5 <83902973+ibrar5@users.noreply.github.com>
2021-10-05 00:32:41 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var p_1 = document.getElementById("player_1").value;
 var p_2 = document.getElementById("player_2").value;
 
-window.localStorage.setItem("player 1" + p_1);
-window.localStorage.setItem("player 2" + p_2);
+window.localStorage.setItem("player 1" , p_1);
+window.localStorage.setItem("player 2" , p_2);
 
 function move(){
     window.location = "game_page.html";
